027bb979489d4496f342352c5ba959e05a6d4099
Mylodie <mylodie7@gmail.com>
2024-01-02 21:24:30 +0800

    feat: renew derp

--- derp/gen_ipranges.py
+++ derp/gen_ipranges.py
@@ -1,36 +1,38 @@
-from netaddr import IPNetwork, IPSet
-import urllib.request
+from urllib.request import Request, urlopen
 
-# Full set, I .e. all possible IPv4 addresses
-full_set = IPSet([IPNetwork('0.0.0.0/0')])
-
-# 给定的 IP 区间列表
-reserved_cidrs = [
-    '0.0.0.0/8',
-    '10.0.0.0/8',
-    '127.0.0.0/8',
-    '169.254.0.0/16',
-    '172.16.0.0/12',
-    '192.168.0.0/16',
-    '224.0.0.0/4',
-    '240.0.0.0/4'
-]
-
-china_cidrs_url = "https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt"
-with urllib.request.urlopen(china_cidrs_url) as response:
-    file_contents = response.read()
-    china_cidrs = [l.decode('utf-8') for l in file_contents.splitlines()] 
-        
-# Subtract the given IP range from the full set.
-for cidr in reserved_cidrs + china_cidrs:
-    full_set.remove(IPNetwork(cidr))
-
-ex_cidrs = [str(iprange) for iprange in full_set.iter_cidrs()]
-
-with open("extraterritorial_ipranges.txt","w+") as f:
-    f.write(",".join(ex_cidrs))
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
 
 
+# lord_alfred_ipranges
+# - google
+# - Bing
+# - AWS
+# - Microsoft(Azure)
+# - Oracle
+# - DO
+# - GH
+# - FB
+# - TW
+# - Linode
+# - TG
+# - OpenAI
 
+with urlopen(
+    Request(
+        url="https://raw.githubusercontent.com/lord-alfred/ipranges/main/all/ipv4_merged.txt",
+        headers=headers,
+    )
+) as response:
+    lord_alfred_ipranges = [l.decode("utf-8") for l in response.read().splitlines()]
+#
 
+with urlopen(
+    Request(url="https://www.cloudflare.com/ips-v4", headers=headers)
+) as response:
+    cloudflare_ipranges = [l.decode("utf-8") for l in response.read().splitlines()]
+#
 
+with open("ex_ipranges.txt", "w+") as f:
+    f.write("\n".join(lord_alfred_ipranges + cloudflare_ipranges))
