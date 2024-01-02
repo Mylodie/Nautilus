56e48561084f0a09c7729d23d5f385fd1f71a6a5
Mylodie <mylodie7@gmail.com>
2024-01-02 22:01:20 +0800

    feat: chore

--- derp/gen_ipranges.py
+++ derp/gen_ipranges.py
@@ -4,35 +4,22 @@ headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
 
+url_list = [
+    "https://raw.githubusercontent.com/lord-alfred/ipranges/main/all/ipv4_merged.txt",
+    "https://www.cloudflare.com/ips-v4",
+]
 
-# lord_alfred_ipranges
-# - google
-# - Bing
-# - AWS
-# - Microsoft(Azure)
-# - Oracle
-# - DO
-# - GH
-# - FB
-# - TW
-# - Linode
-# - TG
-# - OpenAI
+ex_ipranges = []
 
-with urlopen(
-    Request(
-        url="https://raw.githubusercontent.com/lord-alfred/ipranges/main/all/ipv4_merged.txt",
-        headers=headers,
-    )
-) as response:
-    lord_alfred_ipranges = [l.decode("utf-8") for l in response.read().splitlines()]
-#
+for url in url_list:
+    with urlopen(
+        Request(
+            url=url,
+            headers=headers,
+        )
+    ) as response:
+        ex_ipranges += [l.decode("utf-8") for l in response.read().splitlines()]
 
-with urlopen(
-    Request(url="https://www.cloudflare.com/ips-v4", headers=headers)
-) as response:
-    cloudflare_ipranges = [l.decode("utf-8") for l in response.read().splitlines()]
-#
-
-with open("ex_ipranges.txt", "w+") as f:
-    f.write("\n".join(lord_alfred_ipranges + cloudflare_ipranges))
+with open("ex_ipranges.txt", "w") as f:
+    f.write("\n".join(ex_ipranges))
+    print(len(ex_ipranges))
